44859c0bc429e1426a956e5c932758a47129cd35
Damian Katz <damiankatz_87@hotmail.com>
2024-02-25 13:19:52 -0300

    testenv prueba extracción urls sitemap

--- testenv.js
+++ testenv.js
@@ -1,4 +1,8 @@
-const {Builder, By, until} = require('selenium-webdriver');
+// Import the necessary Selenium WebDriver modules
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+// Define the class name of the div element containing the URLs
+const divClassName = 'content-par.responsivegrid';
 
 // Define the function to extract URLs from the div element
 async function extractUrls() {
@@ -9,18 +13,20 @@ async function extractUrls() {
         await driver.get('https://www.mastercard.com.mx/es-mx/sitemap.html');
         
         // Wait for the div element to be present
-        await driver.wait(until.elementLocated(By.className('.site-map .column-wrapper')), 10000);
+        await driver.wait(until.elementLocated(By.css('.' + divClassName)), 10000);
         
         // Find the div element by its class name
-        let divElement = await driver.findElement(By.className('.site-map .column-wrapper'));
-        console.log(divElement)
+        let divElement = await driver.findElement(By.css('.' + divClassName));
+        
         // Find all anchor elements (links) within the div
         let linkElements = await divElement.findElements(By.css('a'));
-        console.log(linkElements)
+        
         // Extract and log the URLs from the anchor elements
         for (let i = 0; i < linkElements.length; i++) {
             let url = await linkElements[i].getAttribute('href');
-            console.log('URL:', url);
+            if (url) {
+                console.log('URL:', url);
+            }
         }
     } finally {
         // Close the WebDriver instance
